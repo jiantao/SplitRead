f880fbef9af8fda72a64ac74873ea820bf284296
Jiantao Wu <jiantaowu.xining@gmail.com>
2011-08-20 14:41:52 -0400

    SR_BamInStream is tested. Bugs are fixed.

--- SR_Map/SR_BamInStream.c
+++ SR_Map/SR_BamInStream.c
@@ -87,6 +87,9 @@ static SR_BamArray* SR_BamArrayAlloc(unsigned int capacity)
     if (pBamArray->data == NULL)
         SR_ErrQuit("ERROR: Not enough memory for the storeage of bam alignments in a bam array object");
 
+    pBamArray->size = 0;
+    pBamArray->capacity = capacity;
+
     return pBamArray;
 }
 
@@ -115,7 +118,7 @@ static void SR_BamArrayStartOver(SR_BamArray* pBamArray)
     pBamArray->size = 1;
 }
 
-static int SR_BamInStreamRead(SR_BamInStream* pBamInStream)
+static int SR_BamInStreamLoadNext(SR_BamInStream* pBamInStream)
 {
     if (pBamInStream->pBamArray->size == pBamInStream->pBamArray->capacity)
     {
@@ -127,6 +130,15 @@ static int SR_BamInStreamRead(SR_BamInStream* pBamInStream)
     ++(pBamInStream->pBamArray->size);
 
     return ret;
+}
+
+static void SR_BamInStreamClear(SR_BamInStream* pBamInStream)
+{
+    pBamInStream->currRefID = NO_QUERY_YET;
+    pBamInStream->currPos = NO_QUERY_YET;
+
+    SR_ARRAY_RESET(pBamInStream->pBamArray);
+    kh_clear(queryName, pBamInStream->nameHash);
 }
 
 
@@ -149,6 +161,8 @@ SR_BamInStream* SR_BamInStreamAlloc(const char* bamFilename)
         SR_ErrQuit("ERROR: Cannot open bam file %s for reading.\n", bamFilename);
 
     pBamInStream->pBamIndex = bam_index_load(bamFilename);
+    if (pBamInStream->pBamIndex == NULL)
+        SR_ErrMsg("WARNING: Cannot open bam index file for reading. No jump allowed.\n");
 
     pBamInStream->currRefID = NO_QUERY_YET;
     pBamInStream->currPos = NO_QUERY_YET;
@@ -166,6 +180,8 @@ void SR_BamInStreamFree(SR_BamInStream* pBamInStream)
         bam_iter_destroy(pBamInStream->pBamIter);
         bam_close(pBamInStream->fpBamInput);
         bam_index_destroy(pBamInStream->pBamIndex);
+
+        free(pBamInStream);
     }
 }
 
@@ -176,8 +192,12 @@ void SR_BamInStreamFree(SR_BamInStream* pBamInStream)
 // jump to a certain chromosome in a bam file
 SR_Status SR_BamInStreamJump(SR_BamInStream* pBamInStream, int32_t refID)
 {
+    // if we do not have the index file return error
+    if (pBamInStream->pBamIndex == NULL)
+        return SR_ERR;
+
     // clear the bam array before jump
-    SR_ARRAY_RESET(pBamInStream->pBamArray);
+    SR_BamInStreamClear(pBamInStream);
 
     // jump and read the first alignment in the given chromosome
     int ret;
@@ -192,20 +212,44 @@ SR_Status SR_BamInStreamJump(SR_BamInStream* pBamInStream, int32_t refID)
     {
         pBamInStream->currRefID = refID;
         pBamInStream->currPos = SR_ARRAY_GET(pBamInStream->pBamArray, 0).core.pos;
-        ret = SR_OK;
+
+        return SR_OK;
+    }
+    else if (ret == -1)
+    {
+        return SR_OUT_OF_RANGE;
     }
     else
-        ret = SR_ERR;
-
-    return ret;
+    {
+        return SR_ERR;
+    }
 }
 
-// load a unique-orphan pair from the bam file
+// read the header of a bam file
+bam_header_t* SR_BamInStreamReadHeader(SR_BamInStream* pBamInStream)
+{
+    return bam_header_read(pBamInStream->fpBamInput);
+}
+
+// read an alignment from a bam file
+SR_Status SR_BamInStreamRead(bam1_t* pAlignment, SR_BamInStream* pBamInStream)
+{
+    int ret = bam_read1(pBamInStream->fpBamInput, pAlignment);
+
+    if (ret > 0)
+        return SR_OK;
+    else if (ret == -1)
+        return SR_EOF;
+    else
+        return SR_ERR;
+}
+
+// load a unique-orphan pair from a bam file
 SR_Status SR_BamInStreamGetPair(bam1_t** ppAnchor, bam1_t** ppOrphan, SR_BamInStream* pBamInStream)
 {
     int ret = 1;
 
-    while((ret > 0) && (ret = SR_BamInStreamRead(pBamInStream)) > 0)
+    while((ret > 0) && (ret = SR_BamInStreamLoadNext(pBamInStream)) > 0)
     {
         // exclude those reads who are non-paired-end, qc-fail, duplicate-marked, proper-paired, 
         // both aligned, secondary-alignment and no-name-specified.
@@ -222,17 +266,18 @@ SR_Status SR_BamInStreamGetPair(bam1_t** ppAnchor, bam1_t** ppOrphan, SR_BamInSt
         if (SR_ARRAY_GET_LAST(pBamInStream->pBamArray).core.tid != pBamInStream->currRefID 
             || SR_ARRAY_GET_LAST(pBamInStream->pBamArray).core.pos != pBamInStream->currPos)
         {
+            // if we got a different reference ID, we should quit
+            if (SR_ARRAY_GET_LAST(pBamInStream->pBamArray).core.tid != pBamInStream->currRefID)
+            {
+                ret = SR_OUT_OF_RANGE;
+            }
+
             pBamInStream->currRefID = SR_ARRAY_GET_LAST(pBamInStream->pBamArray).core.tid;
             pBamInStream->currPos   = SR_ARRAY_GET_LAST(pBamInStream->pBamArray).core.pos;
 
             kh_clear(queryName, pBamInStream->nameHash);
             SR_BamArrayStartOver(pBamInStream->pBamArray);
 
-            // if we got a different reference ID, we should quit
-            if (SR_ARRAY_GET_LAST(pBamInStream->pBamArray).core.tid != pBamInStream->currRefID)
-            {
-                ret = SR_OUT_OF_RANGE;
-            }
         }
 
         int khRet = 0;
--- SR_Map/SR_BamInStream.h
+++ SR_Map/SR_BamInStream.h
@@ -42,20 +42,6 @@ SR_BamInStream* SR_BamInStreamAlloc(const char* bamFilename);
 void SR_BamInStreamFree(SR_BamInStream* pBamInStream);
 
 
-//==================
-// Inline functions
-//==================
-
-//===============================================================
-// function:
-//      see if we could jump in the bam file
-//
-// args:
-//      1. pBamInStream: a pointer to an bam instream structure
-//=============================================================== 
-#define SR_BamCanJump(pBamInStream) ((pBamInStream)->pBamIndex != NULL)
-
-
 //======================
 // Interface functions
 //======================
@@ -75,12 +61,49 @@ SR_Status SR_BamInStreamJump(SR_BamInStream* pBamInStream, int32_t refID);
 
 //================================================================
 // function:
+//      read the header of a bam file
+//
+// args:
+//      1. pBamInStream: a pointer to an bam instream structure
+// 
+// return:
+//      if reading succeeds, return a pointer to a header
+//      structure; if error is found, return NULL
+//
+// discussion: 
+//      The file position indicator must be placed at the 
+//      beginning of the file. Upon success, the position 
+//      indicator will be set at the start of the first alignment
+//================================================================ 
+bam_header_t* SR_BamInStreamReadHeader(SR_BamInStream* pBamInStream);
+
+//================================================================
+// function:
+//      read an alignment from the bam file
+//
+// args:
+//      1. pAlignment: a pointer to an alignment
+//      2. pBamInStream: a pointer to an bam instream structure
+// 
+// return:
+//      if reading succeeds, return SR_OK; if reach the end of
+//      file, return SR_EOF; if get error, return SR_ERR
+//
+// discussion: 
+//      The file position indicator must be placed at the 
+//      beginning of an alignment. Upon success, the position 
+//      indicator will be set at the start of the next alignment
+//================================================================ 
+SR_Status SR_BamInStreamRead(bam1_t* pAlignment, SR_BamInStream* pBamInStream);
+
+//================================================================
+// function:
 //      load a unique-orphan pair from the bam file
 //
 // args:
 //      1. ppAnchor: a pointer of pointer to the anchor mate
 //      2. ppOrphan: a pointer of pointer to the orphan mate
-//      2. pBamInStream : a pointer to an bam instream structure
+//      3. pBamInStream : a pointer to an bam instream structure
 //
 // return:
 //      if we get a unique-orphan pair, return SR_OK; if we reach
